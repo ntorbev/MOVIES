a2472b5b258e4799c33b264c11e1fd27263dbb92
nikolay.torbev <ntorbev@gmail.com>
2018-06-06 00:57:04 +0300

    handle result from service and showing only title from matched movies

---
+++ src/app/core/movie.ts
@@ -0,0 +1,10 @@
+export class Movie {
+  constructor(
+    public title: string,
+    public language: string,
+    public overview: string,
+    public poster: string,
+    public releaseYear: string
+  ) {
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
-import { debounceTime } from 'rxjs/operators';
+import { MovieFindService } from 'src/app/core/movie-find.service';
+import { Movie } from 'src/app/core/movie';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +12,25 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   searchInput: FormControl = new FormControl('');
+  movies: Movie[] = [];
+  myError: any;
 
-  constructor() {
+
+  constructor(private movieFindService: MovieFindService) {
   }
 
   ngOnInit(): void {
-    this.searchInput.valueChanges.pipe(debounceTime(300)).subscribe(() => {
-      // TODO call service for movie search
-    });
+    this.searchInput.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged())
+      .subscribe((value) => {
+        this.movies.length = 0;
+        this.movieFindService.find(value).subscribe(data => {
+            data.forEach(item => item.results.forEach(it => {
+              this.movies.push(new Movie(it.original_title, it.original_language, it.overview, it.poster_path, it.release_date));
+            }));
+          },
+          (err) => this.myError = err);
+      });
   }
 }
